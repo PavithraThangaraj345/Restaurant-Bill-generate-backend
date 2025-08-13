94d0d0f47ac2b020124407ef2250c773c7a5d3d4
PavithraThangaraj345 <pavithrathangaraj345@gmail.com>
2025-08-13 12:54:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,5 +38,9 @@ app.use('/api/employees', employeeRoutes);
 // Serve static files from the 'uploads/avatars' directory
 app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads', 'avatars')));
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
